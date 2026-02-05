c6ede2a14f310c76d05a0e485786e247dc85ba99
Jordan Young <jordanyoung@Jordans-MacBook-Pro.local>
2026-02-05 15:41:42 -0600

    more shape stuff

--- geometry/shapes.py
+++ geometry/shapes.py
@@ -23,6 +23,7 @@ class Triangle(Shape):
         return 0.5 * (self.width * self.height)
 
 
+
 class RightTriangle(Triangle):
     def __init__(self, width, height, center_x = 0, center_y = 0):
         super().__init__(width, height, center_x, center_y)
@@ -32,10 +33,20 @@ class RightTriangle(Triangle):
         self.hyp = math.sqrt((self.a ** 2) + (self.b ** 2))
     
     def get_coordinates(self):
-        pass
+
+        width_radius = self.width / 2
+        height_radius = self.height / 2
+
+        vertex_0 = (self.center_x, self.center_y + height_radius)
+        vertex_1 = (self.center_x - width_radius, self.center_y - height_radius)
+        vertex_2 = (self.center_x + width_radius, self.center_y - height_radius)
+
+        self.coordinates = [vertex_0, vertex_1, vertex_2]
+
+        return self.coordinates
 
 
-class Quad(Shape):
+class Rectangle(Shape):
     def __init__(self, height: float, width: float, center_x: float= 0.0, center_y: float = 0.0):
         super().__init__(center_x, center_y)
 
@@ -47,6 +58,10 @@ class Quad(Shape):
 
     def area(self):
         return self.width * self.height
+
+
+    def perimeter(self) -> float:
+        return 2 * (self.width + self.height)
 
     def get_coordinates(self) -> list[tuple]:
         if self.coordinates:
@@ -65,8 +80,19 @@ class Quad(Shape):
 
 
 
-class Square(Quad):
+class Square(Rectangle):
     def __init__(self, side_length: float, center_x: float = 0, center_y: float = 0.0):
         super().__init__(side_length, side_length, center_x, center_y)
 
 
+class Circle(Shape):
+    def __init__(self, radius: float = 1.0,center_x = 0.0, center_y = 0.0):
+        super().__init__(center_x, center_y)
+        self.radius = radius
+    
+    def area(self) -> float:
+        return math.pi * (self.radius **2)
+    
+    def circumference(self) -> float:
+        return 2 * math.pi * self.radius
+    
